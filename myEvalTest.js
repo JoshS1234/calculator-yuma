e90d577315b7fdd98b99d05da9b428267c6afac7
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2024-03-28 17:52:30 +0000

    added some calculator styling for mobile version, updated myEval so that it correctly does mult/div simultaneously (left to right) then add/subtract (left/right), then did various bug fixes for positive/negative number situations

--- myEvalTest.js
+++ myEvalTest.js
@@ -1,47 +1,59 @@
-const splitCalc = ["1", "*", "2", "+", "3", "-", "8", "/", "4"];
+const splitCalc = ["1", "*", "2", "+", "3", "-", "8", "/", "4", "+", "√4"];
 const strCalc = splitCalc.join(" ");
 
-const myEval = (calcString: string): number => {
+const myEval = (calcString) => {
   let calcArr = calcString.split(" ");
-
-  while (calcArr.includes("*")) {
-    const pos = calcArr.indexOf("*");
-    const miniAns = parseFloat(calcArr[pos - 1]) * parseFloat(calcArr[pos + 1]);
-    calcArr = calcArr
-      .slice(0, pos - 1)
-      .concat([`${miniAns}`])
-      .concat(calcArr.slice(pos + 2));
+  if (calcArr.length == 2) {
+    return parseFloat(calcString);
   }
 
-  while (calcArr.includes("/")) {
-    const pos = calcArr.indexOf("/");
-    const miniAns = parseFloat(calcArr[pos - 1]) / parseFloat(calcArr[pos + 1]);
+  calcArr = calcArr.map((element) => {
+    if (element[0] == "√") {
+      return Math.sqrt(parseFloat(element.slice(1))).toString();
+    } else {
+      return element;
+    }
+  });
+
+  while (calcArr.includes("*") || calcArr.includes("/")) {
+    let i = 0;
+    while (calcArr[i] !== "*" && calcArr[i] !== "/") {
+      i++;
+    }
+
+    let miniAns;
+    if (calcArr[i] == "*") {
+      miniAns = parseFloat(calcArr[i - 1]) * parseFloat(calcArr[i + 1]);
+    } else if (calcArr[i] == "/") {
+      miniAns = parseFloat(calcArr[i - 1]) / parseFloat(calcArr[i + 1]);
+    }
+
     calcArr = calcArr
-      .slice(0, pos - 1)
+      .slice(0, i - 1)
       .concat([`${miniAns}`])
-      .concat(calcArr.slice(pos + 2));
+      .concat(calcArr.slice(i + 2));
   }
 
-  while (calcArr.includes("+")) {
-    const pos = calcArr.indexOf("+");
-    const miniAns = parseFloat(calcArr[pos - 1]) + parseFloat(calcArr[pos + 1]);
-    calcArr = calcArr
-      .slice(0, pos - 1)
-      .concat([`${miniAns}`])
-      .concat(calcArr.slice(pos + 2));
-  }
+  while (calcArr.includes("+") || calcArr.includes("-")) {
+    let i = 0;
+    while (calcArr[i] !== "+" && calcArr[i] !== "-") {
+      i++;
+    }
 
-  while (calcArr.includes("-")) {
-    const pos = calcArr.indexOf("-");
-    const miniAns = parseFloat(calcArr[pos - 1]) - parseFloat(calcArr[pos + 1]);
+    let miniAns;
+    if (calcArr[i] == "+") {
+      miniAns = parseFloat(calcArr[i - 1]) + parseFloat(calcArr[i + 1]);
+    } else if (calcArr[i] == "-") {
+      miniAns = parseFloat(calcArr[i - 1]) - parseFloat(calcArr[i + 1]);
+    }
+
     calcArr = calcArr
-      .slice(0, pos - 1)
+      .slice(0, i - 1)
       .concat([`${miniAns}`])
-      .concat(calcArr.slice(pos + 2));
+      .concat(calcArr.slice(i + 2));
   }
 
   return parseFloat(calcArr[0]);
 };
 
-recursiveCalc(strCalc);
-console.log(recursiveCalc(strCalc));
+console.log(myEval(strCalc));
